e08fb446ef4e635c1f19c87535665437e7c3b594
Viktor <gexar47@gmail.com>
2022-05-01 16:09:58 +0300

    7

--- lendingapp/lending/models.py
+++ lendingapp/lending/models.py
@@ -36,6 +36,7 @@ class Technologies(models.Model):
     title = models.CharField(blank=False, max_length=50, verbose_name='Наименование технологии')
     image = models.ImageField(blank=False, upload_to='techs/', verbose_name='Изображение технологии')
     about = models.TextField(blank=False, max_length=350, verbose_name='О технологии')
+    #titlealt = models.TextField(blank=True, max_length=350, verbose_name='для тега alt')
 
     def __str__(self):
         return self.title
@@ -50,6 +51,7 @@ class Works(models.Model):
     itis = models.CharField(max_length=300, choices = CHOICES, verbose_name='Вид работы')
     image = models.ImageField(blank=False, upload_to='works/', verbose_name='Изображение')
     address = models.CharField(blank=True, max_length=250, verbose_name='Адрес')
+    #titlealt = models.TextField(blank=True, max_length=350, verbose_name='для тега alt')
 
     def __str__(self):
         return self.title
