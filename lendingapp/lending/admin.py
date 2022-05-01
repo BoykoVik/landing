7262cb1d8a851e7d901d771d20144c306d808a20
Viktor <gexar47@gmail.com>
2022-05-01 14:12:40 +0300

    4

--- lendingapp/lending/admin.py
+++ lendingapp/lending/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Services, Servicesdop, Technologies, Works
 # Register your models here.
+
+class ServicesAdmin(admin.ModelAdmin):# настройка отображения в админке
+    list_display = ('title', 'about',)
+
+admin.site.register(Services, ServicesAdmin)
+
+class ServicesdopAdmin(admin.ModelAdmin):# настройка отображения в админке
+    list_display = ('title', 'about',)
+
+admin.site.register(Servicesdop, ServicesdopAdmin)
+
+class TechnologiesAdmin(admin.ModelAdmin):# настройка отображения в админке
+    list_display = ('title', 'image','about',)
+
+admin.site.register(Technologies, ServicesdopAdmin)
+
+class WorksAdmin(admin.ModelAdmin):# настройка отображения в админке
+    list_display = ('title', 'itis', 'image', 'address')
+
+admin.site.register(Works, ServicesdopAdmin)
--- lendingapp/lending/models.py
+++ lendingapp/lending/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+CHOICES = [
+    ('1', 'Сайт'),
+    ('2', 'Дизайн'),
+    ]
+
+class Services(models.Model):
+    title = models.CharField(blank=False, max_length=50, verbose_name='Наименование услуги')
+    about = models.CharField(blank=False, max_length=250, verbose_name='О услуге')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+        ordering = ['-id']
+
+
+class Servicesdop(models.Model):
+    title = models.CharField(blank=False, max_length=50, verbose_name='Наименование услуги')
+    about = models.CharField(blank=False, max_length=250, verbose_name='О услуге')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Доп услуга'
+        verbose_name_plural = 'Доп услуги'
+        ordering = ['-id']
+
+class Technologies(models.Model):
+    title = models.CharField(blank=False, max_length=50, verbose_name='Наименование технологии')
+    image = models.ImageField(upload_to='techs/', verbose_name='Изображение')
+    about = models.CharField(blank=False, max_length=250, verbose_name='О технологии')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Технология'
+        verbose_name_plural = 'Технологии'
+        ordering = ['-id']
+
+class Works(models.Model):
+    title = models.CharField(blank=False, max_length=50, verbose_name='Наименование технологии')
+    itis = models.CharField(max_length=300, choices = CHOICES, verbose_name='Вид работы')
+    image = models.ImageField(upload_to='works/', verbose_name='Изображение')
+    address = models.CharField(blank=True, max_length=250, verbose_name='Адрес')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пример работы'
+        verbose_name_plural = 'Примеры работ'
+        ordering = ['-id']
